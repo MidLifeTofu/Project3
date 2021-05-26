f24ad008d0d36f99dea126cf597e648bd9b8c62c
Jay <jgporter87@outlook.com>
2021-05-26 11:13:32 +1000

    part 3 udpate

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 const data = require('./data.js')
 
+// Step 1
 app.get('/', (req, res) => {
     res.send('Welcome to our schedule website')
 })
@@ -15,6 +16,25 @@ app.get('/schedules', (req, res) => {
     console.log(data.schedules)
 })
 
+// Step 2
+
+
+// Step 3
+app.get('/users/:id/schedules', (req, res) => { //change id param to only digits - tdl
+    const id = Number(req.params.id)
+    let schedules = []
+
+    for (let i = 0; i < data.schedules.length; i++) {
+        let currentSchedule = data.schedules[i]
+        if (currentSchedule.user_id === id) {
+            schedules.push(currentSchedule)
+        }
+    }
+    res.send(schedules)
+})
+
+
+// Step 4
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
