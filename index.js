a136d0ca98c300bacd0ad02996c7424f414196a1
Jay <jgporter87@outlook.com>
2021-05-26 14:19:56 +1000

    progress on part 3

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+// Step 1
 const express = require('express')
 const app = express()
 const port = 3000
+//accesses the information in the data.js file
 const data = require('./data.js')
 
-// Step 1
+//body passer
+app.use(express.json()) 
+app.use(express.urlencoded({extended: true}))
+
+
+// Step 2
 app.get('/', (req, res) => {
     res.send('Welcome to our schedule website')
 })
@@ -16,10 +23,26 @@ app.get('/schedules', (req, res) => {
     console.log(data.schedules)
 })
 
-// Step 2
-
 
 // Step 3
+app.get('/users/:id', (req,res) => {
+    const id = req.params.id
+    let user = []
+
+// go through the users, if the input (/:id) matches the [i] of a user, display their details.
+    for (let i = 0; i < data.users.length; i++) {
+
+        if (data.users[i] === id) {      //something is happening these lines to stop it
+            console.log(data.users[i])     //from bringing up information found in loop
+        }
+    }
+
+    console.log(user)               //these lines are only logging [] because info
+    res.send(user)                  //from if statement is not logging.
+
+})
+
+
 app.get('/users/:id/schedules', (req, res) => { //change id param to only digits - tdl
     const id = Number(req.params.id)
     let schedules = []
@@ -30,11 +53,26 @@ app.get('/users/:id/schedules', (req, res) => { //change id param to only digits
             schedules.push(currentSchedule)
         }
     }
+    console.log(schedules)
     res.send(schedules)
 })
 
 
 // Step 4
+app.post('/schedules', (req, res) => {
+    data.schedules.push(req.body) //push the respond part into the array
+    res.send(req.body)
+})
+
+app.post('/users', (req, res) => {
+    const plainTextPassword = req.body.password
+        console.log(plainTextPassword)
+    bcrypt.hash(plainTextPassword, saltRounds, (eer, hash) => {
+        res.send('Testing')
+    })
+})
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
